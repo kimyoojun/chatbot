2007acdaea0d6305126cb8d22dcf264e8305424c
김유준 <anano3134@gmail.com>
2023-09-26 12:47:23 +0900

    s

---
+++ python/pytorch/torch37.py
@@ -0,0 +1,89 @@
+from __future__ import unicode_literals, print_function, division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import numpy as np
+import pandas as pd
+
+import os
+import re
+import random
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+
+SOS_token = 0
+EOS_token = 1
+MAX_LENGTH = 20
+
+class Lang:
+    def __init__(self):
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2
+    
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+
+
+def normalizeString(df, lang):
+    sentence = df[lang].str.lower()
+    sentence = sentence.str.replace('[^A-Za-z\s]+', ' ')
+    sentence = sentence.str.normalize('NFD')
+    sentence = sentence.str.encode('ascii', errors='ignore').str.decode('utf-8')
+    return sentence
+
+def read_sentence(df, lang1, lang2):
+    sentence1 = normalizeString(df, lang1)
+    sentence2 = normalizeString(df, lang2)
+    return sentence1, sentence2
+
+def read_file(loc, lang1, lang2):
+    df = pd.read_csv(loc, delimiter='\t', header=None, names=[lang1, lang2])
+    return df
+
+def process_data(lang1, lang2):
+    df = read_file('data2/%s-%s.txt' % (lang1, lang2), lang1, lang2)
+    sentence1, sentence2 = read_sentence(df, lang1, lang2)
+
+    input_lang = Lang()
+    output_lang = Lang()
+    pairs = []
+    for i in range(len(df)):
+        if len(sentence1[i].split(' ')) < MAX_LENGTH and len(sentence2[i].split(' ')) < MAX_LENGTH:
+            full = [sentence1[i], sentence2[i]]
+            input_lang.addSentence(sentence1[i])
+            output_lang.addSentence(sentence2[i])
+            pairs.append(full)
+
+    return input_lang, output_lang, pairs
+
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+def tensorsFromPair(input_lang, output_lang, pair):
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return(input_tensor, target_tensor)
